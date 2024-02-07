29e3730a069c0a911c49f367842e16300a85f343
mrjayantbokade <mrjayant81@gmail.com>
2024-02-07 06:56:45 +0530

    cookie-parser and cors

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import { config } from "dotenv"
 import {DB_NAME} from "./constants.js"
 import connectDB from "./db/dbConnection.js"
 import express from "express"
+import { error } from "console"
 
 dotenv.config({
     path: "./env"
@@ -20,10 +21,20 @@ connectDB()
 .then(() =>{
 
     app.get('/', (req, res) =>{
-        res.send("hello")
+        res.send("hello bhai jan ")
     })
 
-    app.listen(process.env.PORT || 8000, () =>{
+
+    //before listening on port 
+    //there might be error came in way 
+    //so to catch those errors we need to write one more function
+
+    app.on("Error OCCURED BEFORE LISTENING ON PORT", (error) =>{
+        console.log("ERRR: ", error);
+        throw error
+    })
+
+    app.listen(process.env.PORT || 5000, () =>{
         console.log(`Application is running on http://localhost:${process.env.PORT}`);
     } )
 })
